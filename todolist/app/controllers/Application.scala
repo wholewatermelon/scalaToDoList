8b321084166ab07613cce300ac1e7fbf0964dada
Kristen <kristen@samwise.local>
2015-10-15 15:58:46 -0600

    Added i18n compliance comments
    Added comments to show where changes were made in the tutorial to
    comply with Scala 2.4 requirements.

--- todolist/app/controllers/Application.scala
+++ todolist/app/controllers/Application.scala
@@ -4,11 +4,12 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import javax.inject.Inject
+import javax.inject.Inject // Inject and i18n are required to make this tutorial compliant with Play 2.4
 import play.api.i18n._
   
 import models.Task
-    
+
+//messageApi is needed to be compliant with I18n, a requirement of Play 2.4
 class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   val taskForm = Form (
@@ -19,6 +20,7 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller wit
     Redirect(routes.Application.tasks)
   }
 
+                        //implicit request required for I18n compliance
   def tasks = Action {  implicit request =>
     Ok(views.html.index(Task.all(), taskForm))
   }
