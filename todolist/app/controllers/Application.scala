5c7b633ac95ebf529d64d89baa67a393a1507249
Kristen <kristen@samwise.local>
2015-10-15 15:31:47 -0600

    Added a task form
    in the ScalaToDoListTutorial, followed the tutorial through the
    “Rendering the first page” task.  Added messagesApi and I18nSupport to
    comply with Play version 2.4

--- todolist/app/controllers/Application.scala
+++ todolist/app/controllers/Application.scala
@@ -2,11 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import javax.inject.Inject
+import play.api.i18n._
+  
+import models.Task
+    
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-class Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  val taskForm = Form (
+    "label" -> nonEmptyText
+    )
+  
+  def index = Action { 
+    Redirect(routes.Application.tasks)
   }
 
+  def tasks = Action {  implicit request =>
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+  
+  def newTask = TODO
+  
+  def deleteTask(id: Long) = TODO
+  
 }
